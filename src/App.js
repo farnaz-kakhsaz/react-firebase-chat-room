84b389437e14fae7cc6adde7ccddd4555aac1de9
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2021-10-22 21:51:38 +0330

    add SingIn, SignOut and ChatRoom components to the main App component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import "./App.css";
-// Firebase SDK:
-import firebase from "firebase/compat/app";
-import "firebase/compat/firestore";
-import "firebase/compat/auth";
+import { firebase, auth, firestore } from "./firebase/firebase";
 // Firebase Hooks:
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useCollectionData } from "react-firebase-hooks/firestore";
-
-firebase.initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-});
-
-const auth = firebase.auth();
-const firestore = firebase.firestore();
+// Components:
+import SignIn from "./components/SignIn";
+import SignOut from "./components/SignOut";
+import ChatRoom from "./components/ChatRoom";
+// CSS:
+import "./App.css";
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">hi</header>
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut auth={auth} />
+      </header>
+      <section>
+        {user ? (
+          <ChatRoom firebase={firebase} firestore={firestore} auth={auth} />
+        ) : (
+          <SignIn firebase={firebase} auth={auth} />
+        )}
+      </section>
     </div>
   );
 }
