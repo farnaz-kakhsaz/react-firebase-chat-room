d2ea59348b52eac02f0246639da2d278055cc2c9
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2021-10-22 21:46:57 +0330

    add firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+// Firebase SDK:
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+export { firebase, auth, firestore };
