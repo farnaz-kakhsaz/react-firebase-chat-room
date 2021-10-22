cf80d765b713176dfd25bf6b16610ae1c95a8bfd
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2021-10-22 21:47:35 +0330

    add SignIn

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,7 @@
+export default function SignIn({ firebase, auth }) {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+  return <button onClick={signInWithGoogle}>Sign In With Google</button>;
+}
